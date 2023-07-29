69fae883228d74243c51aded9e8d76638c44d07b
IrfanKurni <irfangokil23@gmail.com>
2023-07-29 11:04:12 +0700

    chore(master): change AuthenticationResponse

---
+++ src/main/java/com/irfan/moneyrecord/auth/dto/UserData.java
@@ -0,0 +1,17 @@
+package com.irfan.moneyrecord.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserData {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+}

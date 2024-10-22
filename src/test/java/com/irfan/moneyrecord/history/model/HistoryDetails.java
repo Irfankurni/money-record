e912d25bcd6a34652a7c36ea72f02bea3e9f5be0
Irfan Kurniawan <irfankurni98@gmail.com>
2024-10-22 10:06:08 +0000

    chore: fix unit test for entity History & HistoryDetails

--- src/test/java/com/irfan/moneyrecord/history/model/HistoryDetails.java
+++ src/test/java/com/irfan/moneyrecord/history/model/HistoryDetails.java
@@ -1,13 +1,18 @@
 package com.irfan.moneyrecord.history.model;
 
+import com.irfan.moneyrecord.factory.LocalDateFactory;
 import org.junit.jupiter.api.Test;
 import org.meanbean.test.BeanTester;
+
+import java.time.LocalDate;
 
 class HistoryDetailsTest {
 
     @Test
     public void getterAndSetterCorrectness() throws Exception {
-        new BeanTester().testBean(HistoryDetails.class);
+        final BeanTester beanTester = new BeanTester();
+        beanTester.getFactoryCollection().addFactory(LocalDate.class, new LocalDateFactory());
+        beanTester.testBean(HistoryDetailsTest.class);
     }
 
 }
--- src/test/java/com/irfan/moneyrecord/history/model/HistoryTest.java
+++ src/test/java/com/irfan/moneyrecord/history/model/HistoryTest.java
@@ -1,13 +1,18 @@
 package com.irfan.moneyrecord.history.model;
 
+import com.irfan.moneyrecord.factory.LocalDateFactory;
 import org.junit.jupiter.api.Test;
 import org.meanbean.test.BeanTester;
+
+import java.time.LocalDate;
 
 class HistoryTest {
 
     @Test
     public void getterAndSetterCorrectness() throws Exception {
-        new BeanTester().testBean(History.class);
+        final BeanTester beanTester = new BeanTester();
+        beanTester.getFactoryCollection().addFactory(LocalDate.class, new LocalDateFactory());
+        beanTester.testBean(History.class);
     }
 
 }
